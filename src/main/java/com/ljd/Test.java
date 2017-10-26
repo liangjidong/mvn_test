cd6a62bf9ceacf10976f35e3c7dfb8fab11b8b0d
ljdgithub <578362952@qq.com>
2017-10-26 23:11:51 +0800

    hello world start

--- src/main/java/com/ljd/Test.java
+++ src/main/java/com/ljd/Test.java
@@ -1,7 +1,22 @@
 package com.ljd;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@EnableAutoConfiguration
 public class Test {
-    public static void main(String[] args){
-        System.out.println("Hello World");
+
+    @RequestMapping("/")
+    @ResponseBody
+    public String home() {
+        return "Hello World";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Test.class, args);
     }
 }
