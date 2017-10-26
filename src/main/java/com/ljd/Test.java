ffba32a265ff19dec667321144703e4f139f04e1
liangjidong <578362952@qq.com>
2017-10-26 09:54:16 +0800

    first commit

---
+++ src/main/java/com/ljd/Test.java
@@ -0,0 +1,7 @@
+package com.ljd;
+
+public class Test {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,7 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+<h1 align="" xml:lang=""></h1>
+<a href="index.jsp"></a>
+</body>
+</html>
