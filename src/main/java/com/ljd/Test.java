181826b67e75b2c700ed5b282281763d5b2f8c7a
ljd <578362952@qq.com>
2017-10-29 22:56:40 +0800

    mockmvc tester

--- src/main/java/com/ljd/Test.java
+++ src/main/java/com/ljd/Test.java
@@ -1,22 +1,42 @@
 package com.ljd;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.stereotype.Controller;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@EnableAutoConfiguration
+import java.util.Arrays;
+
+@RestController
+@SpringBootApplication
 public class Test {
 
     @RequestMapping("/")
-    @ResponseBody
     public String home() {
         return "Hello World";
     }
 
+    @RequestMapping("/index")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Test.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(final ApplicationContext applicationContext) {
+        return args -> {
+            System.out.println("Let's inspect the beans provided by Spring Boot");
+            String[] beanNames = applicationContext.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            for (String beanName : beanNames) {
+                System.out.println(beanName);
+            }
+        };
     }
 }
